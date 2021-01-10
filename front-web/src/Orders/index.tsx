b29e2b622a60587823181b2c749cd16d66902146
biel-fredy <biel1801fredy@gmail.com>
2021-01-09 21:27:23 -0300

    feat: product list, product card, requisition to spring server

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../api";
+import ProducstList from "./ProductsList";
 import StepsHeader from "./StepsHeader";
+import { Product } from "./types";
 
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+    
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProducstList products={products}/>
         </div>
     );
 }
